2b0006e9d9da6d9f63f9dec5e3def7a8c5b1985e
JorgeRojas827 <jorgerojaspoma09@gmail.com>
2024-07-09 15:36:34 -0500

    feat: 🎸 Setup swagger config
    * Added swagger lib * Added swagger config generator
    
    ✅ Closes: #86du2ymne

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { swaggerConfigGenerator } from '__lib__/swagger-config';
 
 async function bootstrap() {
   const logger = new Logger('Main');
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const config = app.get<ConfigService>(ConfigService);
+  swaggerConfigGenerator(app);
 
   const port = config.get<number>('PORT');
   await app.listen(port, () => logger.log(`Server started on port ${port}`));
